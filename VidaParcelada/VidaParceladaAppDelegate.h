02c69643e4fa3b56fbd5410e45b6b75674981c95
Lauri P. Laux Jr <Junior@Junior-Mac.local>
2012-05-03 21:29:26 -0300

    Initial Commit

---
+++ VidaParcelada/VidaParceladaAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VidaParceladaAppDelegate.h
+//  VidaParcelada
+//
+//  Created by Lauri Paulo Laux Junior Laux on 03/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VidaParceladaAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VidaParcelada/VidaParceladaViewController.m
@@ -0,0 +1,34 @@
+//
+//  VidaParceladaViewController.m
+//  VidaParcelada
+//
+//  Created by Lauri Paulo Laux Junior Laux on 03/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "VidaParceladaViewController.h"
+
+@interface VidaParceladaViewController ()
+
+@end
+
+@implementation VidaParceladaViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
